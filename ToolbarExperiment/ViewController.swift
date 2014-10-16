4c46c6a0ff15cc5ad1c595b0b479a0a75cb225c5
Stefan Arentz <stefan@arentz.ca>
2014-10-16 08:19:15 -0400

    Initial Commit

---
+++ ToolbarExperiment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToolbarExperiment
+//
+//  Created by Stefan Arentz (Mozilla) on 2014-10-16.
+//  Copyright (c) 2014 Stefan Arentz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
